b4b59acd7982af13ca71a197d65c32331cf7601f
Zeusofsky <sibiriwolf@gmail.com>
2023-10-18 08:50:27 +0330

    Update urls.py

--- main/projects/urls.py
+++ main/projects/urls.py
@@ -21,21 +21,6 @@ router.register('api/criticalActions', CriticalActionAPI, basename='criticalActi
 
 urlpatterns = [
     path('api/reportDates', ReportDateAPIEx.as_view()),
-    
-    path('api/reportConfirm/projectManagerReportConfirm/<int:contract_id>/<int:date_id>/<int:confirmed>/', ReportConfirmAPI.as_view({"post": "projectManagerReportConfirm"})),    
-    path('api/financialInfos/contractMonthList/<int:contract_id>/<int:date_id>/', FinancialInfoAPI.as_view({"get": "contractMonthList"})),    
-    path('api/hses/contractMonthList/<int:contract_id>/<int:date_id>/', HseAPI.as_view({"get": "contractMonthList"})),    
-    path('api/progressStates/contractMonthList/<int:contract_id>/<int:date_id>/', ProgressStateAPI.as_view({"get": "contractMonthList"})),
-    path('api/timeProgressStates/contractMonthList/<int:contract_id>/<int:date_id>/', TimeProgressStateAPI.as_view({"get": "contractMonthList"})),
-    path('api/invoices/contractMonthList/<int:contract_id>/<int:date_id>/', InvoiceAPI.as_view({"get": "contractMonthList"})),
-    path('api/financialInvoices/contractMonthList/<int:contract_id>/<int:date_id>/', FinancialInvoiceAPI.as_view({"get": "contractMonthList"})),
-    path('api/workVolumes/contractMonthList/<int:contract_id>/<int:date_id>/', WorkVolumeAPI.as_view({"get": "contractMonthList"})),
-    path('api/pmsProgresses/contractMonthList/<int:contract_id>/<int:date_id>/', PmsprogressAPI.as_view({"get": "contractMonthList"})),
-    path('api/budgetCosts/contractMonthList/<int:contract_id>/<int:date_id>/', BudgetCostAPI.as_view({"get": "contractMonthList"})),
-    path('api/machineries/contractMonthList/<int:contract_id>/<int:date_id>/', MachineryAPI.as_view({"get": "contractMonthList"})),
-    path('api/projectPersonals/contractMonthList/<int:contract_id>/<int:date_id>/', ProjectPersonalAPI.as_view({"get": "contractMonthList"})),
-    path('api/problems/contractMonthList/<int:contract_id>/<int:date_id>/', ProblemAPI.as_view({"get": "contractMonthList"})),
-    path('api/criticalActions/contractMonthList/<int:contract_id>/<int:date_id>/', CriticalActionAPI.as_view({"get": "contractMonthList"})),
 ]
 
 urlpatterns += router.urls
